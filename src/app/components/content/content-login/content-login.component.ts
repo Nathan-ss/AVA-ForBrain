91fe87c712e3b2b42732dd416596b788cc43c005
Nathan Soares Silva <nathan.fnss@gmail.com>
2021-04-27 12:52:48 -0300

    criaçao da tela login

--- src/app/components/content/content-login/content-login.component.ts
+++ src/app/components/content/content-login/content-login.component.ts
@@ -2,22 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 
+
 @Component({
   selector: 'app-content-login',
   templateUrl: './content-login.component.html',
   styleUrls: ['./content-login.component.scss'],
 })
 export class ContentLoginComponent implements OnInit {
-  titulo: string = ' Titulo da Pagina';
+  titulo: string = "login aluno";
+  colorStudent: string = "white";
+  colorTeacher: string = "#0f41b4";
+  actStudent: boolean = true;
+  actTeacher: boolean = null;
 
-
-  pagina1(){
-    
-    this.titulo = ' pagina 1';
+  changeStudent(){
+    this.titulo =  "login aluno";
+    document.getElementById("btn-student").style.background = "#ffff";
+    document.getElementById("btn-student").style.color = "black";
+    document.getElementById("btn-teacher").style.background = "#0f41b4";
+    document.getElementById("btn-teacher").style.color = "#ffff";
+    this.colorTeacher = "#0f41b4";
+    this.colorStudent = "#ffff";
+    this.actStudent =  true;
+    this.actTeacher = false;
     
   }
-  pagina2(){
-    this.titulo = ' pagina 2';
+  changeTeacher(){
+    this.titulo =  "login professor";
+    document.getElementById("btn-student").style.background = "#0f41b4";
+    document.getElementById("btn-student").style.color = "#ffff";
+    document.getElementById("btn-teacher").style.background = "#ffff";
+    document.getElementById("btn-teacher").style.color = "black";
+    this.actStudent = false;
+    this.actTeacher =  true;
+
     
   }
 
