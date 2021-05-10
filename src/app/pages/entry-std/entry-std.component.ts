94f60a37da8b53352a4e85edb1250689ba8af887
Nathan Soares Silva <nathan.fnss@gmail.com>
2021-05-10 12:03:12 -0300

    trabalhando na drawer e no header na pagina entry

--- src/app/pages/entry-std/entry-std.component.ts
+++ src/app/pages/entry-std/entry-std.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 
 @Component({
   selector: 'app-entry-std',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entry-std.component.scss']
 })
 export class EntryStdComponent implements OnInit {
-
+  actbtn : boolean = true;
+  pagename: string = "Home";
+  
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  
+  submitname(name){
+    this.pagename = name;
+
+  }
+
+  action (value){
+    this.actbtn = value;
   }
 
 }
