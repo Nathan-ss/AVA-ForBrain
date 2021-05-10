0320af8893e38b2d5c80bf34712541c576d26e91
Nathan Soares Silva <nathan.fnss@gmail.com>
2021-05-10 19:31:04 -0300

    style the drawer

--- src/app/pages/entry-std/entry-std.component.ts
+++ src/app/pages/entry-std/entry-std.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, } from '@angular/core';
 export class EntryStdComponent implements OnInit {
   actbtn : boolean = true;
   pagename: string = "Home";
-  
+  elementList = <HTMLElement>document.querySelector('.sidebtn');
   
   constructor() { }
 
@@ -19,6 +19,14 @@ export class EntryStdComponent implements OnInit {
   submitname(name){
     this.pagename = name;
 
+  }
+
+  select(name){
+    name.prototype.toString()
+    this.elementList.style.background = "#ffff";
+    this.elementList.style.color = "#2875c7";
+    document.getElementById(name).style.background = "#1750d1";
+    document.getElementById(name).style.color = "black";
   }
 
   action (value){
